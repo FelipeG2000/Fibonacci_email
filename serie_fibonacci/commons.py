4b994da3dc1bf80c7469bd888b920e77a08b50d0
Pipe <pipegarcia1918@hotmail.com>
2024-05-06 14:01:53 -0500

    last update

--- serie_fibonacci/commons.py
+++ serie_fibonacci/commons.py
@@ -3,6 +3,10 @@
 def fibonacci(produced_numbers, first_number=0, second_number=1):
     if second_number < first_number:
         first_number, second_number = second_number, first_number
+
+    if produced_numbers <= 0:
+        produced_numbers = 1
+
     fibonacci_numbers = [first_number, second_number]
     for i in range(produced_numbers+1):
         fibonacci_numbers.append(fibonacci_numbers[-1] + fibonacci_numbers[-2])
--- serie_fibonacci/tests.py
+++ serie_fibonacci/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .commons import fibonacci
+
+
+class FibonacciTestCase(TestCase):
+    def test_fibonacci_positive_numbers(self):
+        produced_numbers = 5
+        result = fibonacci(produced_numbers)
+        expected_result = [0, 1, 1, 2, 3, 5, 8]
+        self.assertEqual(result, expected_result)
+
+    def test_fibonacci_negative_numbers(self):
+        produced_numbers = -5
+        result = fibonacci(produced_numbers)
+        expected_result = [0]
+        self.assertEqual(result, expected_result)
